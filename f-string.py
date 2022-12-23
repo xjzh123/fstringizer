90519d2acaabd896dc3f76fa69a3e8153adeabc5
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2022-12-23 21:19:52 +0800

    支持双引号；支持以变量开头

--- f-string.py
+++ f-string.py
@@ -1,32 +1,54 @@
-def f_string(exp): # 'a' + time + 'b' + nick + awa
+def f_string(exp):  # 'a' + time + 'b' + nick + awa
     stack1 = []
-    isInString = False
+    isInString = True if exp.startswith('"') or exp.startswith('\'') else False
+    currentType = exp[0] if isInString else ''
     lastI = 0
+    stringState = []
     for i in range(len(exp)):
         char = exp[i]
-        if (char == "'" or i == len(exp)-1) and i != 0:
-            if isInString:
-                stack1.append(exp[lastI:i+1])
+
+        #print(isInString, char)
+        if (char == "'" or char == '"' or i == len(exp)-1) and i != 0:
+
+            if i != len(exp)-1:
+                if (isInString and currentType == char):
+                    currentType = ''
+                    stack1.append(exp[lastI+1:i])
+                elif not isInString:
+                    currentType = char
+                    stack1.append(exp[lastI:i+1])
+                else:
+                    continue
+
             else:
-                stack1.append(exp[lastI+1:i])
+                if isInString:
+                    stack1.append(exp[lastI+1:i])
+                else:
+                    currentType = char
+                    stack1.append(exp[lastI:i+1])
+
+            stringState.append(isInString)
             isInString = not isInString
             lastI = i
-    
+
+    # print(stack1)
+    # print(stringState)
+
     # yield stack1 # ['a', "' + time + '", 'b', "' + nick + awa"]
 
     stack2 = []
-    
+
     for i in range(len(stack1)):
         part = stack1[i]
-        if i % 2 == 0:
+        if stringState[i]:
             stack2.append(part)
         else:
-            part = part.strip('\' +')
-            part = part.replace(' ','')
+            part = part.strip('\'\" +')
+            part = part.replace(' ', '')
             parts = part.split('+')
             for p in parts:
                 stack2.append(tuple([p]))
-    
+
     # yield stack2 # ['a',('time'),'b',('nick')]
 
     fstring = "f'"
@@ -37,10 +59,13 @@ def f_string(exp): # 'a' + time + 'b' + nick + awa
             fstring += part
         elif type(part) is tuple:
             fstring += '{' + part[0] + '}'
-    
+
     fstring += "'"
 
     return fstring
 
 
-...
+print(f_string("'a' + time + 'b' + nick + awa"))  # f'a{time}b{nick}{awa}'
+print(f_string("a + time + 'b' + nick + awa"))
+print(f_string("'a' + time + 'b' + nick + 'awa'"))  # f'a{time}b{nick}awa'
+print(f_string("'a\"a\"' + time + \"b\" + nick + awa")) # f'a"a"{time}b{nick}{awa}'
